4cb001defe187ca04334b488e1d2a431def84fea
rigwild <me@rigwild.dev>
2020-09-29 00:35:36 +0200

    Add MQTT listener file handler

---
+++ cmd/mqttfile/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/rigwild/FIL-A2-infrastructures-integration/pkg/mqttfile"
+
+func main() {
+	mqttfile.RunMqttListenerFile()
+}
---
+++ pkg/mqttfile/file.go
@@ -0,0 +1,63 @@
+package mqttfile
+
+import (
+	"fmt"
+	"log"
+
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+
+	"github.com/rigwild/FIL-A2-infrastructures-integration/pkg/putils"
+	"github.com/spf13/viper"
+)
+
+var mqttclient mqtt.Client
+
+func writeFile(filePath string, txt string) {
+	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+	if _, err := f.WriteString(txt + "\n"); err != nil {
+		log.Println(err)
+	}
+}
+
+func onMessageReceived(client mqtt.Client, message mqtt.Message) {
+	payload := message.Payload()
+	spayload := string(payload)
+	fmt.Printf("%s <-- %s\n", message.Topic(), payload)
+
+	t := putils.TimeToDate(putils.ExtractDateFromMsg(spayload))
+	aita, sensor := putils.ExtractAitaSensorFromMsg(spayload)
+
+	filePath := filepath.Join(viper.GetString("listeners.file_logs_dir"), aita+"-"+t+"-"+sensor+".csv")
+	writeFile(filePath, spayload)
+}
+
+// RunMqttListenerFile will run a MQTT client which will save incoming IoT messages to CSV files
+func RunMqttListenerFile() {
+	// mqtt.DEBUG = log.New(os.Stdout, "", 0)
+	// mqtt.ERROR = log.New(os.Stdout, "", 0)
+	putils.LoadConfig()
+
+	// mkdir -p logs_dir
+	os.MkdirAll(viper.GetString("listeners.file_logs_dir"), os.ModePerm)
+
+	// Connect to the MQTT broker
+	_mqttclient := putils.NewMqttClient(viper.GetString("listeners.redis_mqtt_id"))
+	mqttclient = _mqttclient
+
+	// MQTT subscribe and react
+	putils.SubscribeAndReact(mqttclient, viper.GetString("mqtt.topic"), onMessageReceived)
+
+	// Kill the process on SIGTERM
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+}
