aff8a8f7008f3f4396ebefe07b4d3d0084d4982f
rigwild <me@rigwild.dev>
2020-10-01 00:27:05 +0200

    Add fake data population

---
+++ README.md
@@ -0,0 +1,58 @@
+## Redis-stored keys
+
+```
+sensor|2020-09-30|ORD|count 222
+sensor|2020-09-30|ORD|pressure|avg 1051.3913544687514
+sensor|2020-09-30|ORD|pressure|count 37
+sensor|2020-09-30|ORD|pressure|data ,{"v":1050.311188,"d":"2020-09-30 23:33:54 +0200 CEST"},{"v":1049.996706,"d":"2020-09-30 23:34:04 +0200 CEST"}
+sensor|2020-09-30|ORD|pressure|max 1052.878002
+sensor|2020-09-30|ORD|pressure|min 1048.08587
+sensor|2020-09-30|ORD|temperature|avg 23.52497999857995
+sensor|2020-09-30|ORD|temperature|count 37
+sensor|2020-09-30|ORD|temperature|data ,{"v":23.331977,"d":"2020-09-30 23:33:54 +0200 CEST"},{"v":20.497356,"d":"2020-09-30 23:34:04 +0200 CEST"}
+sensor|2020-09-30|ORD|temperature|max 27.907284
+sensor|2020-09-30|ORD|temperature|min 19.006259
+sensor|2020-09-30|ORD|wind|avg 5.989501117821749
+sensor|2020-09-30|ORD|wind|count 37
+sensor|2020-09-30|ORD|wind|data ,{"v":6.823899,"d":"2020-09-30 23:33:54 +0200 CEST"},{"v":5.929727,"d":"2020-09-30 23:34:04 +0200 CEST"}
+sensor|2020-09-30|ORD|wind|max 7.75502
+sensor|2020-09-30|ORD|wind|min 4.004205
+sensor|2020-09|ORD|count 111
+sensor|2020-09|ORD|pressure|avg 1051.3913544687514
+sensor|2020-09|ORD|pressure|count 37
+sensor|2020-09|ORD|pressure|max 1052.878002
+sensor|2020-09|ORD|pressure|min 1048.08587
+sensor|2020-09|ORD|temperature|avg 23.52497999857995
+sensor|2020-09|ORD|temperature|count 37
+sensor|2020-09|ORD|temperature|max 27.907284
+sensor|2020-09|ORD|temperature|min 19.006259
+sensor|2020-09|ORD|wind|avg 5.989501117821749
+sensor|2020-09|ORD|wind|count 37
+sensor|2020-09|ORD|wind|max 7.75502
+sensor|2020-09|ORD|wind|min 4.004205
+sensor|2020|ORD|count 111
+sensor|2020|ORD|pressure|avg 1051.3913544687514
+sensor|2020|ORD|pressure|count 37
+sensor|2020|ORD|pressure|max 1052.878002
+sensor|2020|ORD|pressure|min 1048.08587
+sensor|2020|ORD|temperature|avg 23.52497999857995
+sensor|2020|ORD|temperature|count 37
+sensor|2020|ORD|temperature|max 27.907284
+sensor|2020|ORD|temperature|min 19.006259
+sensor|2020|ORD|wind|avg 5.989501117821749
+sensor|2020|ORD|wind|count 37
+sensor|2020|ORD|wind|max 7.75502
+sensor|2020|ORD|wind|min 4.004205
+sensor|total|ORD|pressure|avg 1051.3913544687514
+sensor|total|ORD|pressure|count 37
+sensor|total|ORD|pressure|max 1052.878002
+sensor|total|ORD|pressure|min 1048.08587
+sensor|total|ORD|temperature|avg 23.52497999857995
+sensor|total|ORD|temperature|count 37
+sensor|total|ORD|temperature|max 27.907284
+sensor|total|ORD|temperature|min 19.006259
+sensor|total|ORD|wind|avg 5.989501117821749
+sensor|total|ORD|wind|count 37
+sensor|total|ORD|wind|max 7.75502
+sensor|total|ORD|wind|min 4.004205
+```
--- pkg/fakeiot/faker.go
+++ pkg/fakeiot/faker.go
@@ -2,6 +2,7 @@ package fakeiot
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
@@ -20,18 +21,48 @@ func mqttConnect() []mqtt.Client {
 }
 
 // publishRaw will publish a message to a mqtt broker
-func publishRaw(client mqtt.Client, msg string) {
-	topic := viper.GetString("mqtt.topic")
-	fmt.Println(topic + " --> " + msg)
+func publishRaw(client mqtt.Client, topic string, msg string) {
+	opts := client.OptionsReader()
+	fmt.Println("[" + opts.ClientID() + "] " + topic + " --> " + msg)
+
 	token := client.Publish(topic, byte(viper.GetInt("mqtt.qos")), false, msg)
 	token.Wait()
 }
 
 // publishRaw will publish a formatted iot message to a mqtt broker
 func publish(client mqtt.Client, sensor string, aita string, data float64) {
-	opts := client.OptionsReader()
-	s := fmt.Sprintf("%s,%s,%s,%f,%s", opts.ClientID(), aita, sensor, data, time.Now().Format(time.RFC3339))
-	publishRaw(client, s)
+	s := fmt.Sprintf("%s,%f,%s", aita, data, time.Now().Format(time.RFC3339))
+	publishRaw(client, viper.GetString("mqtt.topic")+"/"+sensor, s)
+}
+
+func fakeData() {
+	putils.LoadConfig()
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(viper.GetString("mqtt.endpoint"))
+	opts.SetClientID("fake-data-1")
+	opts.SetCleanSession(true)
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	c := 0
+	for i := 10; i <= 30; i++ { // Day
+		for k := 6; k < 9; k++ { // Month
+			for l := 2017; l < 2020; l++ { // Year
+				for j := 0; j < 5; j++ {
+					t := fmt.Sprint(time.Now().Format(time.RFC3339))
+					t = strings.Split(t, "T")[1]
+					t = fmt.Sprint(l) + "-0" + fmt.Sprint(k) + "-" + fmt.Sprint(i) + "T" + t
+					publishRaw(client, viper.GetString("mqtt.topic")+"/pressure", fmt.Sprintf("%s,%f,%s", putils.Aita[j], putils.RandFloat(1048, 1053), t))
+					publishRaw(client, viper.GetString("mqtt.topic")+"/temperature", fmt.Sprintf("%s,%f,%s", putils.Aita[j], putils.RandFloat(19, 28), t))
+					publishRaw(client, viper.GetString("mqtt.topic")+"/wind", fmt.Sprintf("%s,%f,%s", putils.Aita[j], putils.RandFloat(4, 8), t))
+					c++
+				}
+			}
+		}
+	}
+	fmt.Println("----")
+	fmt.Println("Sent " + fmt.Sprint(c) + " messages!")
 }
 
 // RunFakeiot will send fake IoT data to the MQTT broker
@@ -41,6 +72,13 @@ func RunFakeiot() {
 	// mqtt.ERROR = log.New(os.Stdout, "", 0)
 
 	putils.LoadConfig()
+
+	// Environment variable to send fake data accross the month
+	if viper.Get("FAKEIOT_POPULATE") == "1" {
+		fakeData()
+		return
+	}
+
 	sleep := viper.GetInt("fakeiot.broadcast_interval")
 
 	// Connect all fake IoT MQTT clients
